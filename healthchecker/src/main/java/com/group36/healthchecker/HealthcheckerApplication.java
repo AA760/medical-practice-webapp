b3c281700c7a18a439a10eef1f552cd2aeef30c5
AA760 <74122877+AA760@users.noreply.github.com>
2020-12-27 00:48:30 +0000

    Auto-generated by Spring Initializr.

---
+++ healthchecker/src/main/java/com/group36/healthchecker/HealthcheckerApplication.java
@@ -0,0 +1,13 @@
+package com.group36.healthchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthcheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthcheckerApplication.class, args);
+	}
+
+}
