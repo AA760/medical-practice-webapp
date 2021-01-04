de7bf93dc128dfcec8faa7a3c9d44710199c7bf2
AA760 <74122877+AA760@users.noreply.github.com>
2021-01-04 19:25:55 +0000

    Log-in functionality for users stored in external database.

--- healthchecker/src/main/java/com/group36/healthchecker/HealthcheckerApplication.java
+++ healthchecker/src/main/java/com/group36/healthchecker/HealthcheckerApplication.java
@@ -2,11 +2,16 @@ package com.group36.healthchecker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import com.group36.healthchecker.repository.*;
 
 @SpringBootApplication
-public class HealthcheckerApplication {
+@EnableJpaRepositories(basePackageClasses = UserRepository.class)
+public class HealthcheckerApplication 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		SpringApplication.run(HealthcheckerApplication.class, args);
 	}
 
