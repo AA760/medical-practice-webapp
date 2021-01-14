d85e8a82bd047ca5eb1ff21a1b8a1f5dbf3841b4
KhinHtet21 <66117380+KhinHtet21@users.noreply.github.com>
2021-01-14 22:10:47 +0000

    Update Address.java

--- healthchecker/src/main/java/com/group36/healthchecker/Address.java
+++ healthchecker/src/main/java/com/group36/healthchecker/Address.java
@@ -1,4 +1,4 @@
-package com.example.groupproject.model;
+package com.group36.healthchecker.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.springframework.data.annotation.CreatedDate;
