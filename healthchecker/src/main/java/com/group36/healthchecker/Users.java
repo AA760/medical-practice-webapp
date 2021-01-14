86ff502d674e293a4362c34814774dff68390882
KhinHtet21 <66117380+KhinHtet21@users.noreply.github.com>
2021-01-14 22:16:17 +0000

    Add files via upload

---
+++ healthchecker/src/main/java/com/group36/healthchecker/Users.java
@@ -0,0 +1,96 @@
+package com.group36.healthchecker.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "users")
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(value = { "createdAt", "updatedAt" }, allowGetters = true)
+
+public class Users implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank
+	private String user_name;
+
+	@NotBlank
+	private String password;
+
+	@NotBlank
+	private String roles;
+
+	@NotBlank
+	private String active;
+
+	public Users() {
+
+	}
+
+	public Users(@NotBlank String user_name, @NotBlank String password, @NotBlank String roles,
+			@NotBlank String active) {
+		super();
+		this.user_name = user_name;
+		this.password = password;
+		this.roles = roles;
+		this.active = active;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUser_name() {
+		return user_name;
+	}
+
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRoles() {
+		return roles;
+	}
+
+	public void setRoles(String roles) {
+		this.roles = roles;
+	}
+
+	public String getActive() {
+		return active;
+	}
+
+	public void setActive(String active) {
+		this.active = active;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+}
