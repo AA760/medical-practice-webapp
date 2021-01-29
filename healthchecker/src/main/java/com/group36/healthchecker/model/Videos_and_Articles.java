8bff73c1548c77f15a0c8cdd9974988df45c009f
KhinHtet21 <66117380+KhinHtet21@users.noreply.github.com>
2021-01-29 13:32:22 +0000

    Add files via upload

---
+++ healthchecker/src/main/java/com/group36/healthchecker/model/Videos_and_Articles.java
@@ -0,0 +1,95 @@
+package com.group36.healthchecker.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "videos")
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(value = { "createdAt", "updatedAt" }, allowGetters = true)
+
+public class Videos_and_Articles implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotNull
+	private String name;
+
+	private String description;
+
+	@NotNull
+	private String link;
+
+	@Column(nullable = false, updatable = false)
+	@Temporal(TemporalType.TIMESTAMP)
+	@CreatedDate
+	private Date upload_date;
+
+	public Videos_and_Articles() {
+
+	}
+
+	public Videos_and_Articles(@NotNull String name, String description, @NotNull String link) {
+		super();
+		this.name = name;
+		this.description = description;
+		this.link = link;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getLink() {
+		return link;
+	}
+
+	public void setLink(String link) {
+		this.link = link;
+	}
+
+	public Date getUpload_date() {
+		return upload_date;
+	}
+
+	public void setUpload_date(Date upload_date) {
+		this.upload_date = upload_date;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+}
