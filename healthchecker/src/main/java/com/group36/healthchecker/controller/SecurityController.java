4cd283054f6e08b68913a78e600eb33bf57e02c3
AA760 <74122877+AA760@users.noreply.github.com>
2021-02-09 17:31:03 +0000

    Added logged in username retrieval

---
+++ healthchecker/src/main/java/com/group36/healthchecker/controller/SecurityController.java
@@ -0,0 +1,21 @@
+package com.group36.healthchecker.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SecurityController 
+{
+
+	//returns username of currently logged in user
+	@RequestMapping(value = "/username", method = RequestMethod.GET)
+	@ResponseBody
+	public String currentUserName(Authentication authentication) 
+	{
+		return authentication.getName();
+		
+	}
+}
