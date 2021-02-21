444280a798a8be9c78d9f00f1c4a8aa1dc6ee270
Tanvir Akhter Shakib <2013158@brunel.ac.uk>
2021-02-21 16:39:37 +0000

    SearchPatientsName

---
+++ healthchecker/src/main/java/com/group36/healthchecker/repository/SearchPatientsName.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ * 
+ * @author tanvirakhtershakib
+ *
+ *
+ */
+
+
+import java.sql.*;
+
+public class SearchPatientsName {
+   public static void main(String[] args) throws Exception {
+      Class.forName("org.apache.derby.jdbc.ClientDriver");
+      Connection con = DriverManager.getConnection(
+         "jdbc:derby://localhost:1527/testDb","username", "password");
+      
+      Statement stmt = con.createStatement();
+      String query[] = {
+         "SELECT * FROM emp where id = 1", 
+         "select name from emp where name like 'tanvir_'", 
+         "select name from emp where name like 'tanvir%'" 
+      };
+      
+      for(String q : query) {
+         ResultSet rs = stmt.executeQuery(q);
+         System.out.println("Search for Patient's name"+q);
+         
+         while (rs.next()) {
+            String name = rs.getString("name");
+            System.out.print(name+"  ");
+         }
+         System.out.println();
+      }
+   }
+}
